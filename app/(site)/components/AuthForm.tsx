a031c40554b75d96e3e0fd83c77b480cfd6798cd
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-07-01 14:02:12 +0500

    add social buttons

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,8 @@ import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
+import {BsGithub, BsGoogle} from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -109,13 +111,39 @@ const AuthForm = () => {
                         type='submit'
                     >
                         {variant === 'LOGIN' ? 'Sign in' : 'Register'}
-                    </Button>
-
-
-                        
-                    
-                    
+                    </Button>    
                 </form>
+
+                <div className="mt-6">
+                    <div className="relative">
+                        <div
+                            className="
+                                absolute
+                                inset-0
+                                flex
+                                items-center
+                            "
+                        >
+                            <div className="w-full border-t border-gray-300"/>
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with 
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+
+                        <AuthSocialButton
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+                </div>
             </div>
             
         </div>

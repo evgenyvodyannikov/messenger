4e4717f0ac32df076aab4c6468a424f7d2c1878e
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-07-01 18:32:54 +0500

    add react toast

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,7 +3,8 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
-import {BsGithub, BsGoogle} from 'react-icons/bs'
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+import { toast } from 'react-hot-toast';
 
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
@@ -42,7 +43,9 @@ const AuthForm = () => {
         setIsLoading(true);
 
         if (variant === 'REGISTER'){
-            axios.post('/api/register', data);
+            axios.post('/api/register', data)
+            .catch(() => toast.error('Something went wrong!'))
+            .finally(() => setIsLoading(false));
         }
         
         if (variant === 'LOGIN'){
